e5ace95f12c5a8d75a4bb733ea9efe9b1d763ff4
Ray's Window PC <raychanray64@gmail.com>
2024-08-03 19:27:24 -0400

    added scroll parallax menu

--- src/compontents/Hero.jsx
+++ src/compontents/Hero.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { useRef } from "react";
 import Section from "./Section";
 import { curve, robot, heroBackground } from "../assets";
 import Button from "./Button";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -12,7 +17,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div
           className="relative z-1 max-w-[62rem] mx-auto 
             text-center mb-[4rem] md:mb-20 lg:mb:[6rem]"
@@ -49,11 +54,21 @@ const Hero = () => {
               >
                 <img
                   src={robot}
-                  className="w-full"
+                  className="w-full scale-[1.7]
+                  translate-y-[8%] md:scale[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
                   width={1024}
                   height={490}
                   alt="AI"
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
           </div>
