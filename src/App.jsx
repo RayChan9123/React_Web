0199711df09905af57889179d98be9e1dac74869
Ray's Window PC <raychanray64@gmail.com>
2024-08-04 14:01:23 -0400

    finished Heading.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./compontents/Benefits";
 import Button from "./compontents/Button";
 import Header from "./compontents/Header";
 import Hero from "./compontents/Hero";
@@ -9,6 +10,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
       </div>
 
       <ButtonGradient />
